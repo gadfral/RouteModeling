74e77adc6c3439f8b5913838d7aee018379b346d
Gadfral <gadfral@mail.ru>
2024-06-18 01:18:22 +0300

    Fix order fields

--- RouteModelingApp/Entities/RouteModel.cs
+++ RouteModelingApp/Entities/RouteModel.cs
@@ -2,9 +2,9 @@
 
 public class RouteModel
 {
-    public string FullPrice { get; set; }
-
+    public string Name { get; set; }
+    
     public string Time { get; set; }
 
-    public string Name { get; set; }
+    public string FullPrice { get; set; }
 }
