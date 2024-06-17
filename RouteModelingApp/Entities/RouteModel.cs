53a931b8c3c9d5b6d431af9a08d22091671661d8
Gadfral <gadfral@mail.ru>
2024-06-17 17:22:37 +0300

    Add entities

---
+++ RouteModelingApp/Entities/RouteModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RouteModelingApp.Entities;
+
+public class RouteModel
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+}
