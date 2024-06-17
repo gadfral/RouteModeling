a287de934af7846301a4115338e4660bece2c20c
Gadfral <gadfral@mail.ru>
2024-06-17 18:58:40 +0300

    Random fix

--- RouteModelingCore/RandomCalc.cs
+++ RouteModelingCore/RandomCalc.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RouteModelingCore.Entities;
 
 namespace RouteModelingCore
 {
     public static class RandomCalc
     {
-
         /// <summary>
         /// Путь в метрах
         /// </summary>
@@ -19,12 +19,26 @@ namespace RouteModelingCore
         /// Скорость в км/ч
         /// </summary>
         /// <returns></returns>
-        public static int RandomSpeed() => new Random().Next(15, 80);
+        public static int RandomSpeed(AutoType type) => type switch
+        {
+            AutoType.PassengerCar => new Random().Next(1000, 10000),
+            AutoType.Suv => new Random().Next(1000, 1000),
+            AutoType.Сargo => new Random().Next(1000, 1000),
+            AutoType.Truck => new Random().Next(1000, 1000),
+            _ => throw new ArgumentOutOfRangeException(nameof(type))
+        };
 
         /// <summary>
         /// Цена за км
         /// </summary>
         /// <returns></returns>
-        public static decimal RanmdonPrice() => new Random().Next(100, 500);
+        public static decimal RandomPrice(AutoType type) => type switch
+        {
+            AutoType.PassengerCar => new Random().Next(1000, 10000),
+            AutoType.Suv => new Random().Next(1000, 1000),
+            AutoType.Сargo => new Random().Next(1000, 1000),
+            AutoType.Truck => new Random().Next(1000, 1000),
+            _ => throw new ArgumentOutOfRangeException(nameof(type))
+        };
     }
 }
